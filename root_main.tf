fcd174820afb186eecf488590e60ac82a0e0a431
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-23 21:42:20 +0530

    Update root_main.tf

--- root_main.tf
+++ root_main.tf
@@ -1,34 +1,16 @@
-# Provider Configuration
 provider "aws" {
   region = var.aws_region
 }
 
-# Generate SSH Key Pair using Terraform (TLS provider)
-resource "tls_private_key" "key_pair" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-# Create an AWS EC2 Key Pair
-resource "aws_key_pair" "key" {
-  key_name   = "webserver-key"
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
 
 # Security Group
-resource "aws_security_group" "web_sg" {
-  name_prefix = "web-sg-"
-
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH traffic"
+  
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # For testing; restrict to your IP for production.
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,18 +21,23 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "EC2SecurityGroup"
+  }
 }
 
 # EC2 Instance
-resource "aws_instance" "web_server" {
+resource "aws_instance" "ec2" {
   ami           = var.aws_ami
   instance_type = var.instance_type
-  key_name      = aws_key_pair.key.key_name
+  security_groups = [aws_security_group.ec2_sg.name]
 
-  security_groups = [
-    aws_security_group.web_sg.name
-  ]
-
+  tags = {
+    Name    = "MyEC2Instance5"
+    Version = var.version_name
+  }
+}
   # Use a provisioner to wait for SSH to be ready
   provisioner "remote-exec" {
     inline = [
@@ -59,7 +46,7 @@ resource "aws_instance" "web_server" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = tls_private_key.key_pair.private_key_pem
+      private_key = aws_security_group.ec2_sg.name
       host        = self.public_ip
     }
   }
