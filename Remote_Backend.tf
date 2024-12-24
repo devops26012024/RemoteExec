dde71fa20a9637ce6c563919b1e8cac78bcdc60e
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-24 18:43:17 +0530

    Update Remote_Backend.tf

--- Remote_Backend.tf
+++ Remote_Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "pradeep-terraform-1234333"
-    key            = "remote-module/terraform.tfstate"
+    key            = "remote-modu/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-demo"
     encrypt        = true
